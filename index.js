30e5321c44c0e73e6d6d1d37caab5efa9a6a4cd2
KevinH48264 <1kevin.huang@gmail.com>
2022-09-09 23:48:58 -0400

    adding rest of index.js

--- index.js
+++ index.js
@@ -31,50 +31,50 @@ app.get('/',(req, res) => {
     res.render('todo.ejs');
 });
 
-// //POST METHOD
-// app.post('/',async (req, res) => {
-//     // create a new task item and try to save it to our database, throw error if there is an error and send us back to root page
-//     const todoTask = new TodoTask({
-//     content: req.body.content
-//     });
-//     try {
-//     await todoTask.save();
-//     res.redirect("/");
-//     } catch (err) {
-//     res.redirect("/");
-//     }
-//     });
+//POST METHOD
+app.post('/',async (req, res) => {
+    // create a new task item and try to save it to our database, throw error if there is an error and send us back to root page
+    const todoTask = new TodoTask({
+    content: req.body.content
+    });
+    try {
+    await todoTask.save();
+    res.redirect("/");
+    } catch (err) {
+    res.redirect("/");
+    }
+    });
 
-// // GET METHOD, just render the todo.ejs template html page
-// app.get("/", (req, res) => {
-//     TodoTask.find({}, (err, tasks) => {
-//     res.render("todo.ejs", { todoTasks: tasks });
-//     });
-// });
+// GET METHOD, just render the todo.ejs template html page
+app.get("/", (req, res) => {
+    TodoTask.find({}, (err, tasks) => {
+    res.render("todo.ejs", { todoTasks: tasks });
+    });
+});
 
-// //UPDATE and have a new page route called /edit/[:id]
-// app.route("/edit/:id").get((req, res) => {
-//     const id = req.params.id;
-//     // render new page with tasks but this time with idTask of the one selected
-//     TodoTask.find({}, (err, tasks) => {
-//         res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
-//     });
-// }).post((req, res) => {
-//     const id = req.params.id;
-//     // update todo task item based on id and updating the content, throw error if there is and send us back to root page
-//     TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
-//     if (err) return res.send(500, err);
-//         res.redirect("/");
-//     });
-// });
+//UPDATE and have a new page route called /edit/[:id]
+app.route("/edit/:id").get((req, res) => {
+    const id = req.params.id;
+    // render new page with tasks but this time with idTask of the one selected
+    TodoTask.find({}, (err, tasks) => {
+        res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+    });
+}).post((req, res) => {
+    const id = req.params.id;
+    // update todo task item based on id and updating the content, throw error if there is and send us back to root page
+    TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+    if (err) return res.send(500, err);
+        res.redirect("/");
+    });
+});
 
-// //DELETE
-// app.route("/remove/:id").get((req, res) => {
-//     const id = req.params.id;
-//     // for the TodoTask model, find the id and remove it using the function, and throw and error if there's an error
-//     TodoTask.findByIdAndRemove(id, err => {
-//     if (err) return res.send(500, err);
-//     res.redirect("/"); // send back to root page
-//     });
-//     });
+//DELETE
+app.route("/remove/:id").get((req, res) => {
+    const id = req.params.id;
+    // for the TodoTask model, find the id and remove it using the function, and throw and error if there's an error
+    TodoTask.findByIdAndRemove(id, err => {
+    if (err) return res.send(500, err);
+    res.redirect("/"); // send back to root page
+    });
+    });
     
